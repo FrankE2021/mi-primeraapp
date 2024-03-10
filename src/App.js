61b5694a183ca38922e78a19d071ae90af044da5
Soy Frank <ernestinversion@gmail.com>
2024-03-10 16:29:13 -0300

    Uploading to GitHub

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import logo from "./assets/logo512.png";
+
+
+function App() {
+  const estilo1 = {backgroundColor: "green", color: "white", fontSize: "2em"}
+  const estilo2 = {backgroundColor: "blue", color: "white", fontSize: "2.5em"}
+  const [contenido, setContenido] = useState ("");
+  const [colores, setColores] = useState (estilo1);
+  const escribir = () => {
+    setContenido('Me llamo Frank')
+  }
+  const imagen = () => {
+    setContenido(<img src={logo} alt=""/>)
+  }
+  const cambioColor = () => {
+    setColores(estilo2)
+  }
+  return (
+    <>
+      <button onClick={() => escribir()}>Boton 1</button>
+      <button onClick={() => imagen()}>Boton 2</button>
+      <button onClick={() => cambioColor()}>Boton 3</button>
+      <div className="caja" style={colores}>
+        {contenido}
+      </div>      
+    </>
+  );
+}
+
+export default App;
